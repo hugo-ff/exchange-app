4fca65a91b0c942ed639ccf01701b561af79896e
Hugo Flotts <hugoflotts@outlook.com>
2023-11-26 06:42:39 -0300

    chore: first commit and project setup

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export function App() {
+	return (
+		<div>
+			<h1 className="text-3xl font-bold underline">Exchange app</h1>
+		</div>
+	);
+}
---
+++ tests/App.spec.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+
+import { App } from "../src/App";
+
+test("App component display header", () => {
+	render(<App />);
+
+	const heading = screen.getByText(/Exchange app/i);
+
+	expect(heading).toBeInTheDocument();
+});
